d06d7f40b8a6538e4aa57e1f32ab6436ad9f91ad
musabekova5 <m_samalka@mail.ru>
2019-02-09 14:51:08 +0600

    task4

---
+++ Week1/task4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[ , ] a = new string[n, n];
+
+           //string[] nums = Console.ReadLine().Split(new char[] { ' ', '*', '[', ']', ','});
+            for (int i = 0; i<n; ++i)
+            {
+                for(int j=0; j<i+1; ++j)
+                {
+                    a[i, j] = "[*]";
+                    Console.Write(a[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
