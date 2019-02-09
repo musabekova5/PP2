7293774e1cb0fb57883d8a72966cd63382dceac4
musabekova5 <m_samalka@mail.ru>
2019-02-09 15:00:41 +0600

    task2

---
+++ Week1/task2/Properties/Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2.Properties
+{
+    class Student
+    {
+        string name = null;
+        public string Name
+        {
+            get //accessor
+            {
+                return name;
+            }
+            set //mutator
+            {
+                name = value;
+            }
+        }
+        public string ID = null;
+        public int year;
+
+        #region constructor
+        public Student()
+        {
+
+        }
+        public Student(string name, string ID, int year)
+        {
+            Name = name;
+            this.ID = ID;
+            this.year = year;
+        }
+        public Student(string name, string a)
+        {
+            Name = name;
+            ID = a;
+
+        }
+        #endregion
+        public void PrintInfo()
+        {
+            Console.WriteLine(Name + " " + ID + " " + year);
+        }
+    }
+}
