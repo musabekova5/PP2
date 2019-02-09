6f560023957e2b376de675793298343407dc40a7
musabekova5 <m_samalka@mail.ru>
2019-02-09 14:50:34 +0600

    task2

---
+++ Week1/task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using task_2.Properties;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st = new Student("Samal", "18BD117379", 0);
+            st.PrintInfo();
+            Student s = new Student("Samal", "18BD117379");
+            s.year = st.year + 1;
+            s.PrintInfo();
+        }
+    }
+}
