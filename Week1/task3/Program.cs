dcaccf520cb63e799ae319a7985b5ea316130818
musabekova5 <m_samalka@mail.ru>
2019-02-09 14:50:55 +0600

    task3

---
+++ Week1/task3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+
+            string[] nums = Console.ReadLine().Split(new char[] { ',', ';', '#', ' ' });
+
+            
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                Console.Write(nums[i] + " " + nums[i] + " ");
+            }
+        }
+    }
+}
