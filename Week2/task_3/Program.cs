86a7ec505912d5dc2f29924397bad32541a9b5de
musabekova5 <m_samalka@mail.ru>
2019-02-14 03:50:18 +0600

    jh

---
+++ Week2/task_3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void PrintInfo(FileSystemInfo sm, int a) // создаю функцию
+        {
+            string s = new string(' ', a);
+            Console.WriteLine(s + sm.Name);
+
+            if (sm.GetType() == typeof(DirectoryInfo)) // если файл является папкой
+            {
+                FileSystemInfo[] arr = ((DirectoryInfo)sm).GetFileSystemInfos(); // иду дальше
+                for (int i = 0; i < arr.Length; ++i)
+                {
+                    PrintInfo(arr[i], a + 3); // вывожу и делаю отступ
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            DirectoryInfo d = new DirectoryInfo(@"C:\Users\Lenovo\Desktop\PP2"); // указываю необходимую папку
+            PrintInfo(d, 0); // вызываю функцию 
+        }
+    }
+}
