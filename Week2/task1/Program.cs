4c3b8d0bfaad8c4465204b24e879ac8bfbe5a979
musabekova5 <m_samalka@mail.ru>
2019-02-14 03:48:30 +0600

    committed

---
+++ Week2/task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        private static string reverse(string p)
+        {
+           
+            char[] s = p.ToCharArray();//перезагрузить в массив символов
+            Array.Reverse(s);
+            return new string(s);//перезагружает его в строку
+        }
+        static void Main(string[] args)
+        {
+            StreamReader r = new StreamReader(@"C:/Users/Lenovo/Desktop/read.txt");//читает файл
+            string p = r.ReadLine();//читает содиржимое, то есть все символы
+            if (p.Equals(reverse(p)))//если р равно своему обратному
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+
+    }
+}
