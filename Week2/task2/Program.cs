5391af53ffdfda2f33f3df550b71a598d305f2f0
musabekova5 <m_samalka@mail.ru>
2019-02-14 03:48:56 +0600

    com

---
+++ Week2/task2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = System.IO.File.ReadAllText(@"C:/Users/Lenovo/Desktop/read.txt");//считывает содиржимое файла
+            string[] p = line.Split(new char[] { ' ', ',' });//разделяю элементы строки 
+            string prime = "";
+            for (int i = 0; i < p.Length; i++)//начинается проверка на простое число 
+            {
+                int cnt = 0;
+                int b = int.Parse(p[i]); //присваиваю элемент
+                for (int j = 1; j < b; j++)
+
+                {
+                    if (b % j == 0)
+                        cnt++;
+                }
+                if (b == 1)
+                {
+                    continue;
+                }
+                if (cnt <= 1)
+                {
+                    prime += b;
+                    prime += " ";
+                }
+            }
+            System.IO.File.WriteAllText(@"C:/Users/Lenovo/Desktop/output.txt", prime);//вывожу ответ на отдельный файл 
+
+        }
+    }
+}
+        
+    
+
