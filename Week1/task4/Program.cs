5a8c6f273beee4648657c7458d626ed0797d1790
musabekova5 <m_samalka@mail.ru>
2019-02-16 06:45:25 +0600

    kjh

--- Week1/task4/Program.cs
+++ Week1/task4/Program.cs
@@ -10,16 +10,16 @@ namespace task_4
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
-            string[ , ] a = new string[n, n];
+            int n = int.Parse(Console.ReadLine());//вводим кол в массиве
+            string[ , ] a = new string[n, n];// двойной массив 
 
            //string[] nums = Console.ReadLine().Split(new char[] { ' ', '*', '[', ']', ','});
-            for (int i = 0; i<n; ++i)
+            for (int i = 0; i<n; ++i)//по строкам
             {
-                for(int j=0; j<i+1; ++j)
+                for(int j=0; j<i+1; ++j)//столбики в строке 
                 {
-                    a[i, j] = "[*]";
-                    Console.Write(a[i, j]);
+                    a[i, j] = "[*]";//даем значение звездочки в квадрате 
+                    Console.Write(a[i, j]);// вывод
                 }
                 Console.WriteLine();
             }
