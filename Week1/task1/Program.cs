bce734d73d5e46f9267d79f6a6146bb5fb6393da
musabekova5 <m_samalka@mail.ru>
2019-02-16 06:44:29 +0600

    yg

--- Week1/task1/Program.cs
+++ Week1/task1/Program.cs
@@ -10,25 +10,25 @@ namespace task_1
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
-            int i, j, p, cnt=0;
-            int[] arr = new int[n];
+            int n = int.Parse(Console.ReadLine());//ввод переменной, сколько чисел будет в массиве
+            int i, j, p, cnt=0;// переменные интежер 
+            int[] arr = new int[n];//массив 
 
-            //Console.Write("Enter array elements :");
-            string[] nums = Console.ReadLine().Split(new char[] { ',', ';', '#', ' ' });
+            
+            string[] nums = Console.ReadLine().Split(new char[] { ',', ';', '#', ' ' });//чтоб учитывать символы как запятая 
 
             for (i = 0; i < arr.Length; i++)
             {
-                arr[i] = int.Parse(nums[i]);
+                arr[i] = int.Parse(nums[i]);//замена на интежер от стринга
             }
 
-           // Console.Write("All Prime List is :");
+       
 
             for (i = 0; i < arr.Length; i++)
             {
                 j = 2;
                 p = 1;
-                while (j < arr[i])
+                while (j < arr[i])//начинается условие проверки на простое число 
                 {
 
                     if (arr[i] % j == 0)
@@ -39,14 +39,14 @@ namespace task_1
                     }
                     j++;
                 }
-                if (arr[i] == 1)
+                if (arr[i] == 1)//пропускаем 1 так как не является простым числом 
                 {
                     continue;
                 }
                     if (p == 1)
                    {
                      cnt++;    
-                      Console.Write(arr[i] + " ");
+                      Console.Write(arr[i] + " ");//выводим просытые числа на экран 
                 }
                    
             }
